50f2617fd8656ab1907d34c40f2c791ec5245337
Uni-Garage <140313358+Uni-Garage@users.noreply.github.com>
2026-03-09 04:42:58 +0000

    Updated evaluate.py

---
+++ Malaria_CNN_Project/src/evaluate.py
@@ -0,0 +1,36 @@
+from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.models import load_model
+import numpy as np
+import matplotlib.pyplot as plt
+from data_preprocessing import load_data
+
+train_data, test_data = load_data()
+
+model = load_model("../models/resnet_model.h5")
+
+pred = model.predict(test_data)
+pred = np.round(pred)
+
+cm = confusion_matrix(test_data.classes, pred)
+
+print("Confusion Matrix")
+print(cm)
+
+print("Classification Report")
+print(classification_report(test_data.classes, pred))
+
+
+plt.imshow(cm, cmap="Blues")
+plt.title("Confusion Matrix")
+plt.colorbar()
+
+classes = ["Parasitized","Uninfected"]
+
+plt.xticks(range(2), classes)
+plt.yticks(range(2), classes)
+
+plt.xlabel("Predicted")
+plt.ylabel("Actual")
+
+plt.savefig("../results/confusion_matrix.png")
+plt.show()
