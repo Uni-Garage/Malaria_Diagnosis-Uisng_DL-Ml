47d570320f89fab0589602a9cfae059ed18680c6
Uni-Garage <140313358+Uni-Garage@users.noreply.github.com>
2026-03-09 04:45:43 +0000

    Update vgg_model.py

---
+++ Malaria_CNN_Project/src/vgg_model.py
@@ -0,0 +1,31 @@
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, Flatten, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+def build_vgg():
+
+    base_model = VGG16(
+        weights="imagenet",
+        include_top=False,
+        input_shape=(224,224,3)
+    )
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+    x = Flatten()(x)
+    x = Dense(256, activation="relu")(x)
+    x = Dropout(0.5)(x)
+    predictions = Dense(1, activation="sigmoid")(x)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    model.compile(
+        optimizer=Adam(learning_rate=0.0001),
+        loss="binary_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
