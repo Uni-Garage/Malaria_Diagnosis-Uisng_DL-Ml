b33e712e69802e4bf286aaac971b4783696c2ea7
Uni-Garage <140313358+Uni-Garage@users.noreply.github.com>
2026-03-11 14:17:35 +0530

    Update test_model.py

--- malaria_web_app_2/test_model.py
+++ malaria_web_app_2/test_model.py
@@ -5,7 +5,7 @@ from utils.preprocess import preprocess_image
 model = load_model("models/vgg_finetuned.h5")
 
 # Preprocess a sample parasitized cell image
-img_array = preprocess_image("D:\Dinesh\Dinesh_test_files\PROJECTS\Me\FinalYEAR\Malaria_CNN_Project2\dataset\Malaria_dataset\Uninfected\C241NThinF_IMG_20151207_124643_cell_125.png")
+img_array = preprocess_image("D:\Malaria_CNN_Project2\dataset\Malaria_dataset\Uninfected\cell_125.png")
 # Make prediction
 prob = model.predict(img_array)[0][0]
 
@@ -13,4 +13,5 @@ print("Predicted probability:", prob)
 
 # Optional: map to label
 prediction = "malaria" if prob > 0.5 else "healthy"
+
 print("Prediction:", prediction)
